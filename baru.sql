c0e564255650e9e92a80a93a281e14308a345b5b
incesamsul <ince.samsul89@gmail.com>
2023-08-24 22:52:02 +0800

    add db

--- baru.sql
+++ baru.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 02, 2023 at 04:55 PM
+-- Generation Time: Aug 24, 2023 at 02:51 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `dpk`
+--
+
+CREATE TABLE `dpk` (
+  `id` int NOT NULL,
+  `id_group` int NOT NULL,
+  `tipe` varchar(100) NOT NULL,
+  `nama` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nominal` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `rate` varchar(100) NOT NULL,
+  `jangka_waktu` varchar(100) NOT NULL,
+  `keterangan` varchar(100) NOT NULL,
+  `tgl_proyeksi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `dpk`
+--
+
+INSERT INTO `dpk` (`id`, `id_group`, `tipe`, `nama`, `nominal`, `rate`, `jangka_waktu`, `keterangan`, `tgl_proyeksi`) VALUES
+(464, 84, 'bumd-giro-cash-in', 'PT MAJU', '10', '5%', '1 Tahun', 'ditunda', '10/10/1922'),
+(465, 84, 'bumd-deposito-cash-in', 'PT JAYA', '90', '10%', '8 Bulan', 'baru', '90/10/2881'),
+(466, 84, 'bumd-giro-cash-out', 'PT MUNDUR', '2', '7%', '7 Bulan', 'baru', '9/9/9291');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `jenis_lembar_kerja`
 --
 
@@ -40,6 +67,42 @@ INSERT INTO `jenis_lembar_kerja` (`id`, `nama`) VALUES
 (1, 'Mikro'),
 (2, 'Ritel'),
 (3, 'Kmg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kredit_produktifitas`
+--
+
+CREATE TABLE `kredit_produktifitas` (
+  `id` int NOT NULL,
+  `id_jenis_lembar_kerja` int NOT NULL,
+  `id_group` int NOT NULL,
+  `cabang` varchar(100) NOT NULL,
+  `bulan` varchar(50) NOT NULL,
+  `tahun` year NOT NULL,
+  `bulan_input` int NOT NULL,
+  `jml_rm` int NOT NULL,
+  `pencarian` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kredit_produktifitas`
+--
+
+INSERT INTO `kredit_produktifitas` (`id`, `id_jenis_lembar_kerja`, `id_group`, `cabang`, `bulan`, `tahun`, `bulan_input`, `jml_rm`, `pencarian`) VALUES
+(61, 1, 84, 'cabang', 'Jun-22', '2018', 2, 99, 88),
+(62, 1, 84, 'cabang', 'Dec-22', '2018', 2, 76, 54),
+(63, 1, 84, 'cabang', 'Mar-23', '2018', 2, 55, 44),
+(64, 1, 84, 'cabang', 'Apr-23', '2018', 2, 33, 44),
+(65, 1, 84, 'cabang', 'May-23', '2018', 2, 55, 66),
+(66, 1, 84, 'cabang', 'Jun-23', '2018', 2, 55, 44),
+(67, 1, 84, 'mks', 'Jun-22', '2018', 2, 56, 56),
+(68, 1, 84, 'mks', 'Dec-22', '2018', 2, 55, 44),
+(69, 1, 84, 'mks', 'Mar-23', '2018', 2, 34, 32),
+(70, 1, 84, 'mks', 'Apr-23', '2018', 2, 76, 55),
+(71, 1, 84, 'mks', 'May-23', '2018', 2, 66, 78),
+(72, 1, 84, 'mks', 'Jun-23', '2018', 2, 12, 32);
 
 -- --------------------------------------------------------
 
@@ -79,38 +142,38 @@ INSERT INTO `kredit_realisasi` (`id`, `id_jenis_lembar_kerja`, `id_group`, `caba
 (233, 3, 87, 'mks', 'apr-23', '2021', 12, 3),
 (234, 3, 87, 'mks', 'mei-23', '2021', 12, 3),
 (235, 3, 87, 'mks', 'jun-23', '2021', 12, 3),
-(236, 1, 84, 'cabang', 'jun-22', '2020', 12, 1),
-(237, 1, 84, 'cabang', 'jul-22', '2020', 12, 1),
-(238, 1, 84, 'cabang', 'dec-22', '2020', 12, 1),
-(239, 1, 84, 'cabang', 'mar-22', '2020', 12, 1),
-(240, 1, 84, 'cabang', 'mar-23', '2020', 12, 1),
-(241, 1, 84, 'cabang', 'apr-23', '2020', 12, 1),
-(242, 1, 84, 'cabang', 'mei-23', '2020', 12, 1),
-(243, 1, 84, 'cabang', 'jun-23', '2020', 12, 11),
-(244, 1, 84, 'jpn', 'jun-22', '2020', 12, 2),
-(245, 1, 84, 'jpn', 'jul-22', '2020', 12, 2),
-(246, 1, 84, 'jpn', 'dec-22', '2020', 12, 2),
-(247, 1, 84, 'jpn', 'mar-22', '2020', 12, 2),
-(248, 1, 84, 'jpn', 'mar-23', '2020', 12, 2),
-(249, 1, 84, 'jpn', 'apr-23', '2020', 12, 2),
-(250, 1, 84, 'jpn', 'mei-23', '2020', 12, 2),
-(251, 1, 84, 'jpn', 'jun-23', '2020', 12, 2),
-(252, 1, 84, 'frnc', 'jun-22', '2020', 12, 123),
-(253, 1, 84, 'frnc', 'jul-22', '2020', 12, 3),
-(254, 1, 84, 'frnc', 'dec-22', '2020', 12, 3),
-(255, 1, 84, 'frnc', 'mar-22', '2020', 12, 3),
-(256, 1, 84, 'frnc', 'mar-23', '2020', 12, 3),
-(257, 1, 84, 'frnc', 'apr-23', '2020', 12, 3),
-(258, 1, 84, 'frnc', 'mei-23', '2020', 12, 3),
-(259, 1, 84, 'frnc', 'jun-23', '2020', 12, 3),
-(260, 1, 84, 'uk', 'jun-22', '2020', 12, 8),
-(261, 1, 84, 'uk', 'jul-22', '2020', 12, 8),
-(262, 1, 84, 'uk', 'dec-22', '2020', 12, 8),
-(263, 1, 84, 'uk', 'mar-22', '2020', 12, 8),
-(264, 1, 84, 'uk', 'mar-23', '2020', 12, 8),
-(265, 1, 84, 'uk', 'apr-23', '2020', 12, 8),
-(266, 1, 84, 'uk', 'mei-23', '2020', 12, 8),
-(267, 1, 84, 'uk', 'jun-23', '2020', 12, 8);
+(332, 1, 84, 'cabang', 'jun-22', '2018', 1, 11),
+(333, 1, 84, 'cabang', 'jul-22', '2018', 1, 1),
+(334, 1, 84, 'cabang', 'dec-22', '2018', 1, 1),
+(335, 1, 84, 'cabang', 'mar-22', '2018', 1, 1),
+(336, 1, 84, 'cabang', 'mar-23', '2018', 1, 1),
+(337, 1, 84, 'cabang', 'apr-23', '2018', 1, 1),
+(338, 1, 84, 'cabang', 'mei-23', '2018', 1, 1),
+(339, 1, 84, 'cabang', 'jun-23', '2018', 1, 1),
+(340, 1, 84, 'jpn', 'jun-22', '2018', 1, 2),
+(341, 1, 84, 'jpn', 'jul-22', '2018', 1, 2),
+(342, 1, 84, 'jpn', 'dec-22', '2018', 1, 2),
+(343, 1, 84, 'jpn', 'mar-22', '2018', 1, 2),
+(344, 1, 84, 'jpn', 'mar-23', '2018', 1, 2),
+(345, 1, 84, 'jpn', 'apr-23', '2018', 1, 2),
+(346, 1, 84, 'jpn', 'mei-23', '2018', 1, 2),
+(347, 1, 84, 'jpn', 'jun-23', '2018', 1, 2),
+(348, 1, 84, 'frnc', 'jun-22', '2018', 1, 123),
+(349, 1, 84, 'frnc', 'jul-22', '2018', 1, 3),
+(350, 1, 84, 'frnc', 'dec-22', '2018', 1, 3),
+(351, 1, 84, 'frnc', 'mar-22', '2018', 1, 3),
+(352, 1, 84, 'frnc', 'mar-23', '2018', 1, 3),
+(353, 1, 84, 'frnc', 'apr-23', '2018', 1, 3),
+(354, 1, 84, 'frnc', 'mei-23', '2018', 1, 3),
+(355, 1, 84, 'frnc', 'jun-23', '2018', 1, 3),
+(356, 1, 84, 'uk', 'jun-22', '2018', 1, 8),
+(357, 1, 84, 'uk', 'jul-22', '2018', 1, 8),
+(358, 1, 84, 'uk', 'dec-22', '2018', 1, 8),
+(359, 1, 84, 'uk', 'mar-22', '2018', 1, 8),
+(360, 1, 84, 'uk', 'mar-23', '2018', 1, 8),
+(361, 1, 84, 'uk', 'apr-23', '2018', 1, 8),
+(362, 1, 84, 'uk', 'mei-23', '2018', 1, 8),
+(363, 1, 84, 'uk', 'jun-23', '2018', 1, 8);
 
 -- --------------------------------------------------------
 
@@ -232,9 +295,21 @@ INSERT INTO `users` (`id`, `nama`, `nrik`, `role`, `no_hp`, `email`, `password`,
 --
 
 --
+-- Indexes for table `dpk`
+--
+ALTER TABLE `dpk`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `jenis_lembar_kerja`
 --
 ALTER TABLE `jenis_lembar_kerja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kredit_produktifitas`
+--
+ALTER TABLE `kredit_produktifitas`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -274,16 +349,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `dpk`
+--
+ALTER TABLE `dpk`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=467;
+
+--
 -- AUTO_INCREMENT for table `jenis_lembar_kerja`
 --
 ALTER TABLE `jenis_lembar_kerja`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `kredit_produktifitas`
+--
+ALTER TABLE `kredit_produktifitas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+
+--
 -- AUTO_INCREMENT for table `kredit_realisasi`
 --
 ALTER TABLE `kredit_realisasi`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=268;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=364;
 
 --
 -- AUTO_INCREMENT for table `lampiran`
